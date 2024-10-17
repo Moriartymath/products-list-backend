18ffbe8e860b955e8d19217c7769de175fa159df
Strelia Illia <seylvin2005@gmail.com>
2024-10-17 11:39:33 +0300

    add cors middleware

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const productRouter = require("./routes/productRoutes");
 const glErrorMiddleware = require("./middlewares/glErrorMiddleware");
 
 app.use(helmet());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/v1/products", productRouter);
